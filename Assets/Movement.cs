046a55fe83f779acb9828003d8f994343d5416fc
Szymon <szymon.wiercigroch@gmail.com>
2022-09-01 20:14:46 +0200

    finished level and main menu

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -8,22 +8,29 @@ public class Movement : MonoBehaviour
 
     public MovementController controller;
     public Rigidbody2D _rb;
-    
+    public Animator animator;
+
     private float _runSpeed = 40f;
     private float _horizontalMove = 0f;
     private float _verticalJumpAdjustment = 100f;
     private bool _discharge = false;
     private float _charger = 0f;
     private float _tempMove = 0f;
+    private static readonly int SideSpeed = Animator.StringToHash("Side_Speed");
+    private static readonly int JumpUp = Animator.StringToHash("Jump_Up");
+    private static readonly int JumpDown = Animator.StringToHash("Jump_Down");
 
 
     // Update is called once per frame
     void Update()
     {
         _horizontalMove = Input.GetAxisRaw("Horizontal") * _runSpeed;
-
+        
+        animator.SetFloat(SideSpeed,Mathf.Abs(_horizontalMove));
+        
         if (Input.GetKey(KeyCode.Space))
         {
+            animator.SetBool(JumpUp,true);
             _tempMove = _horizontalMove;
             _charger += Time.deltaTime;
             _horizontalMove = 0f;
@@ -31,7 +38,18 @@ public class Movement : MonoBehaviour
         
         if (Input.GetKeyUp(KeyCode.Space))
         {
+            animator.SetBool(JumpUp,true);
             _discharge = true;
+        }
+
+        if (_rb.velocity.y < 0f && Math.Abs(_horizontalMove) > 0f)
+        {
+            animator.SetBool(JumpUp,false);
+            animator.SetBool(JumpDown,true);
+        }
+        else
+        {
+            animator.SetBool(JumpDown,false);
         }
     }
 
@@ -44,5 +62,6 @@ public class Movement : MonoBehaviour
             _charger = 0f;
         }
         controller.Move(_horizontalMove * Time.fixedDeltaTime, false);
+
     }
 }
